c5e9aab40b955c31aa884005bdd285a0f51c4f2f
KirilAngov <kangov@collectorsquest.com>
2011-11-04 14:32:35 +0000

    Removing old plugins

--- plugins/sfPropelActAsTaggableBehaviorPlugin/config/sfPropelActAsTaggableBehaviorPluginConfiguration.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class sfPropelActAsTaggableBehaviorPluginConfiguration extends sfPluginConfiguration
-{
-  public function initialize()
-  {
-    require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'config.php');
-    return parent::initialize();
-  }
-}
--- plugins/sfRequestHistoryPlugin/lib/helper/RequestHistoryHelper.php
+++
@@ -1,7 +0,0 @@
-<?php
-function get_go_back_button($label = 'Go Back', $num = 1)
-{
-  $num = $num * -1;
-
-  return button_to($label, sfRequestHistory::goUrl($num));
-}
