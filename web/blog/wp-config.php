5cd9277095d844243fff959392a2bb94a04319c3
KirilAngov <kangov@collectorsquest.com>
2012-01-10 14:26:41 +0000

    Updating two WordPress plugins

--- web/blog/wp-config.php
+++ web/blog/wp-config.php
@@ -1,18 +1,12 @@
 <?php
 
-include dirname(__FILE__) .'/../../config/secure/wp-config.php';
-
-define('SECRET_KEY', 'ipx$\"\'GQ!;[Mm`~X}wNat}FG#&S7}8C MsqoNbx/ii8,QL~=.I*BMlm>%i_` H>[');
-define('WP_CACHE', true);
+include_once __DIR__ .'/../../config/wp-config.php';
 
 // You can have multiple installations in one database if you give each a unique prefix
 $table_prefix  = 'wp_';   // Only numbers, letters, and underscores please!
 
-// Change this to localize WordPress.  A corresponding MO file for the
-// chosen language must be installed to wp-includes/languages.
-// For example, install de.mo to wp-includes/languages and set WPLANG to 'de'
-// to enable German language support.
-define ('WPLANG', '');
+define('WPLANG', '');
+define('WP_CACHE', true);
 
 /* That's all, stop editing! Happy blogging. */
 
